6666f0ae6b0d92718c64f21c28c3a785476edbe6
238-ally <3636732273@qq.com>
2026-03-16 21:10:21 +0800

    Site updated: 2026-03-16 21:10:21

---
+++ js/banner.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: { delay: 5000, disableOnInteraction: false },
+    pagination: { el: ".swiper-pagination", clickable: true },
+    navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
+    effect: 'fade'
+  });
+});

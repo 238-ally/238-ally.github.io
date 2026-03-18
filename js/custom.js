0029c5ae2bae432778264c7f8240b18dc4bac3e1
238-ally <3636732273@qq.com>
2026-03-18 19:51:58 +0800

    Site updated: 2026-03-18 19:51:57

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-  if (document.getElementById("typed")) {
-    new Typed("#typed", {
-      strings: [
-        "Welcome to my blog world",
-        "Hexo + Butterfly"
-      ],
-      typeSpeed: 60,
-      backSpeed: 30,
-      loop: true
-    });
-  }
+  const el = document.getElementById("typed");
+  if (!el) return;
+
+  new Typed("#typed", {
+    strings: [
+      "I love it",
+      "Hexo + Butterfly"
+    ],
+    typeSpeed: 60,
+    backSpeed: 30,
+    loop: true
+  });
 });
